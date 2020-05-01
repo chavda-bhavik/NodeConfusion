971d965b6047d13d39c169a5032e0d7d4c57634c
chavda-bhavik <bhavik.chvda@gmail.com>
2020-05-01 10:39:47 +0530

    Implemented OAuth with facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'jwt-secret': 'bv-chavda',
     'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '3017557368304369',
+        clientSecret: '6dda4aeb0561c9fe9b2b4f4d137a935f' 
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -59,4 +59,13 @@ router.get('/logout', (req,res) => {
   }
 })
 
+router.get('/facebook/token', passport.authenticate('facebook-token'), (req,res) => {
+  if(req.user) {
+    var token = authenticate.generateToken({_id: req.user._id });
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({ success: true, token, status: 'Login Successful!' })
+  }
+})
+
 module.exports = router;
